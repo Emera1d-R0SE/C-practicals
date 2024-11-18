f4b4ee3a8c3b7557d81939f2688b5b1135bf9f74
Emera1d-R0SE <emerald42rose@outlook.com>
2024-11-18 14:06:22 +0530

    create program16.c

--- program16.c
+++ program16.c
@@ -2,15 +2,15 @@
 #include<stdio.h>
 int main()
 {
-    int a=0, b=1, temp, n=10;
-    printf("fibbonacci series:%d %d", a, b );
+    int num1=0, num2=1, temp, n=10;
+    printf("fibbonacci series:%d %d", num1, num2 );
     int fibonacci();
     for(int i=3; i<=n; ++i)
     {
-        temp=a+b;
+        temp=num1+num2;
         printf("%d",temp);
-        a=b;
-        b=temp;
+        num1=num2;
+        num2=temp;
     }
     return 0;
 }
