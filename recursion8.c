b3b1ea1e3c7cf0b909be19a5167a524effe2a639
Emera1d-R0SE <emerald42rose@outlook.com>
2024-11-18 12:58:30 +0530

    Create recursion8.c

---
+++ recursion8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("enter size..: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("enter elements: ");
+    for(int i=0;i<n;i++){
+    scanf("%d", &arr[i]);}
+    printf("The largest element in the array is %d", findLargest(arr,n));
+    return 0;
+}
+int findLargest(int arr[], int n) {
+    if (n == 1) {
+        return arr[0];
+    }
+    if (arr[n - 1] > findLargest(arr, n-1)) {
+        return arr[n - 1];
+    } else {
+        return findLargest(arr, n-1);
+    }
+}
